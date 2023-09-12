802af7d0bd02010f0b05fb68ceb354296e13df8c
monique <moniquejp16@gmail.com>
2023-09-12 16:34:28 -0300

    fix: atualizando para poder rodar cada pasta de cada vez

---
+++ aula-ts-03/src/index.ts
@@ -0,0 +1,31 @@
+function multiply(num1:number, num2: number) : number{
+    return num1 * num2;
+}
+
+const resultMult = multiply(2, 3);
+console.log("multiplicação: ", resultMult);
+
+function sum(num1:number, num2: number) : number{
+    return num1 * num2;
+}
+
+const resultSum = sum(2, 3);
+console.log("somatoria: ", resultSum);
+
+function isEven(num: number): boolean{
+    return num % 2 === 0;
+}
+
+const resultIsEven = isEven(2);
+console.log("par/impar: ", resultIsEven);
+
+function showResult(result: number) : void {
+    if(isEven(result)) {
+        console.log(`The result is ${result} and it's even!`);
+    } else {
+        console.log(`The result is ${result} and it's not even!`);
+        
+    }
+}
+
+const resultShowResult = showResult(3);
